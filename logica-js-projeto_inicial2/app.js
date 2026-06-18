b9b21603aa8ca0ab8895136a10ed11d6eec9255e
Gustavo Batista <145714578+gustavo1506@users.noreply.github.com>
2026-06-18 11:08:29 -0300

    fdd

--- logica-js-projeto_inicial2/app.js
+++ logica-js-projeto_inicial2/app.js
@@ -1,3 +1,5 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -35,7 +37,21 @@ function verificarChute() {
 }
 
 function gerarNumeroAleatorio(){
-    return parseInt(Math.random()*10+1);
+    let numeroEscolhido = parseInt(Math.random()*numeroLimite+1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+    if (quantidadeDeElementosNaLista == numeroLimite){
+        listaDeNumerosSorteados = [];
+    }
+
+    if(listaDeNumerosSorteados.includes(numeroEscolhido)){
+        return gerarNumeroAleatorio();
+    } else{
+        listaDeNumerosSorteados.push
+        (numeroEscolhido)
+        console.log(listaDeNumerosSorteados)
+        return numeroEscolhido
+    }
 }
 
 function limparCampo(){
@@ -44,7 +60,7 @@ function limparCampo(){
 }
 
 function reiniciarJogo(){
-    numeroSecreto = gerarNumeroAleatorio;
+    numeroSecreto = gerarNumeroAleatorio();
     limparCampo();
     tentativas = 1;
     exibirMensagemInicial();
